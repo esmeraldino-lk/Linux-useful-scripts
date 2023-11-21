18ee2d4483a29ca9aabf79aad2a574e7d8611524
esmeraldino-lk <133903212+esmeraldino-lk@users.noreply.github.com>
2023-11-21 13:41:55 -0300

    Add files via upload

---
+++ tree-forense-scan-1.sh
@@ -0,0 +1,12 @@
+#bin/bash
+
+tree -f -R | grep pkexec
+echo done pkexec.
+tree -f -R | grep crackmapexec
+echo done crackmapexec
+tree -f -R | grep nmap
+echo done nmap
+tree -f -R | grep fscan
+echo done fscan
+
+echo Done
